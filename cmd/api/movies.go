4d15341d08e9eb3979661026db1ec920acd98661
rejath chandran <rejathchandran@rejaths-MacBook-Air.local>
2023-01-23 15:22:47 +0530

    completed marshalling part and error response

---
+++ cmd/api/movies.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"greenlight_api_project/internal/data"
+	"net/http"
+	"time"
+)
+
+func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "create a new movie")
+}
+
+func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request) {
+	id, err := app.readIDParam(r)
+	if err != nil {
+		app.notFoundResponse(w, r)
+		return
+	}
+
+	movie := data.Movie{
+		ID:        id,
+		CreatedAt: time.Now(),
+		Title:     "Casablanca",
+		Runtime:   102,
+		Genres:    []string{"drama", "romance", "war"},
+		Version:   1,
+	}
+
+	err = app.writeJSON(w, http.StatusOK, movie, nil)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+	}
+}
---
+++ internal/data/movies.go
@@ -0,0 +1,13 @@
+package data
+
+import "time"
+
+type Movie struct {
+	ID        int64     `json:"id"`
+	CreatedAt time.Time `json:"-"` // Use the - directive
+	Title     string    `json:"title"`
+	Year      int32     `json:"year,omitempty"`           // Add the omitempty directive
+	Runtime   Runtime   `json:"runtime,omitempty,string"` // Add the omitempty directive
+	Genres    []string  `json:"genres,omitempty"`         // Add the omitempty directive
+	Version   int32     `json:"version"`
+}
